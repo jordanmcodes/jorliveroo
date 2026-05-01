a75e5c1f570b33e696095d356f97870997163303
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-05-02 00:12:33 +0100

    Cleaned up files, removing duplicates

--- jorliveroo/deliveryproject/menu/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Category, FoodItem
-
-admin.site.register(Category)
-admin.site.register(FoodItem)
--- jorliveroo/deliveryproject/menu/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import FoodItem
-
-def menu(request):
-    items = FoodItem.objects.all()
-    return render(request, 'menu/menu.html',{'items: items'})
-
