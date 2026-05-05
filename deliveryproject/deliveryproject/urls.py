1e1cf8b10d55793bc14f79bbb333b9a135b62f8b
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-05-05 09:18:30 +0100

    linked burgers button to django page so that when the button is pressed, the server links to the burgers.html page.

--- deliveryproject/deliveryproject/urls.py
+++ deliveryproject/deliveryproject/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from menu import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('menu/', views.menu, name='menu'),
+    path('burgers/', views.burgers, name ='burgers'),
 ]
+
+
