fe78f664ebf7a365846dcd8babce6c067d6af317
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-05-05 11:16:49 +0100

    Linked every page to the database, pulling items from each individual category.

--- deliveryproject/menu/views.py
+++ deliveryproject/menu/views.py
@@ -6,37 +6,49 @@ def menu(request):
     return render(request, 'menu/menu.html',{'items': items})
 
 def burgers (request):
-    return render (request, 'menu/burgers.html')
+    items = FoodItem.objects.filter(category__name="Burgers")
+    return render (request, 'menu/burgers.html', {'items': items} )
 
 def pasta (request):
-    return render (request, 'menu/pasta.html')
+    items = FoodItem.objects.filter(category__name="Pasta")
+    return render (request, 'menu/pasta.html', {'items': items})
 
 def pizza (request):
-    return render (request, 'menu/pizza.html')
+    items = FoodItem.objects.filter(category__name="Pizza")
+    return render (request, 'menu/pizza.html', {'items': items})
 
 def fish (request):
-    return render (request, 'menu/fish.html')
+    items = FoodItem.objects.filter(category__name="Fish")
+    return render (request, 'menu/fish.html', {'items': items})
 
 def soup (request):
-    return render (request, 'menu/soup.html')
+    items = FoodItem.objects.filter(category__name="Soup")
+    return render(request, 'menu/soup.html',{'items': items})
 
 def sides (request):
-    return render (request, 'menu/sides.html')
+    items = FoodItem.objects.filter(category__name="Sides")
+    return render (request, 'menu/sides.html', {'items': items})
 
 def curry (request):
-    return render (request, 'menu/curry.html')
+    items = FoodItem.objects.filter(category__name="Curry")
+    return render (request, 'menu/curry.html', {'items': items})
 
 def vegan (request):
-    return render (request, 'menu/vegan.html')
+    items = FoodItem.objects.filter(category__name="Vegan")
+    return render (request, 'menu/vegan.html', {'items': items})
 
 def gluten_free (request):
-    return render (request, 'menu/gluten_free.html')
+    items = FoodItem.objects.filter(category__name="Gluten Free")
+    return render (request, 'menu/gluten_free.html', {'items': items} )
 
 def saver_menu (request):
-    return render (request, 'menu/saver_menu.html')    
+    items = FoodItem.objects.filter(category__name="Saver Menu")
+    return render (request, 'menu/saver_menu.html', {'items': items})   
 
 def desserts (request):
-    return render (request, 'menu/desserts.html')
+    items = FoodItem.objects.filter(category__name="Desserts")
+    return render (request, 'menu/desserts.html', {'items': items} )
 
 def kids_menu (request):
-    return render (request, 'menu/kids_menu.html')
+    items = FoodItem.objects.filter(category__name="Kids Menu")
+    return render (request, 'menu/kids_menu.html',{'items': items} )
