c23e7816b0c7211a37ccf40487970d4562b12a0e
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-05-22 17:24:38 +0100

    added allowed hosts

--- deliveryproject/deliveryproject/settings.py
+++ deliveryproject/deliveryproject/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = "dummy key"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 # Application definition
 
