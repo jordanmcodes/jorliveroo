9acf3e1ad6fd8dfd697558d4c1b693a43c0c2cba
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-05-22 11:45:12 +0100

    I added sessions to all of the categories, copying the same format that I did for burgers in views.py

--- deliveryproject/menu/views.py
+++ deliveryproject/menu/views.py
@@ -17,89 +17,129 @@ def burgers (request):
     return render (request, 'menu/burgers.html', {'items': items, 'chosen_item': chosen_item} )
 
 def pasta (request):
+    basket = request.session.get('basket', [])
     chosen_item = None
     if request.method == "POST":
         item_id = request.POST.get ("item_id")
+        basket.append(item_id)
+        request.session['basket'] = basket
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Pasta")
     return render (request, 'menu/pasta.html', {'items': items, 'chosen_item': chosen_item} )
 
 def pizza (request):
+    basket = request.session.get('basket', [])
     chosen_item = None
     if request.method == "POST":
         item_id = request.POST.get ("item_id")
+        basket.append(item_id)
+        request.session['basket'] = basket
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Pizza")
     return render (request, 'menu/pizza.html', {'items': items, 'chosen_item': chosen_item} )
 
 def fish (request):
+    basket = request.session.get('basket', [])
     chosen_item = None
     if request.method == "POST":
         item_id = request.POST.get ("item_id")
+        basket.append(item_id)
+        request.session['basket'] = basket
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Fish")
     return render (request, 'menu/fish.html', {'items': items, 'chosen_item': chosen_item} )
 
 def soup (request):
+    basket = request.session.get('basket', [])
     chosen_item = None
     if request.method == "POST":
         item_id = request.POST.get ("item_id")
+        basket.append(item_id)
+        request.session['basket'] = basket
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Soup")
-    return render(request, 'menu/soup.html',{'items': items, 'chosen_item': chosen_item} )
+    return render (request, 'menu/soup.html', {'items': items, 'chosen_item': chosen_item} )
 
 def sides (request):
+    basket = request.session.get('basket', [])
     chosen_item = None
     if request.method == "POST":
         item_id = request.POST.get ("item_id")
+        basket.append(item_id)
+        request.session['basket'] = basket
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Sides")
     return render (request, 'menu/sides.html', {'items': items, 'chosen_item': chosen_item} )
 
 def curry (request):
+    basket = request.session.get('basket', [])
     chosen_item = None
     if request.method == "POST":
         item_id = request.POST.get ("item_id")
+        basket.append(item_id)
+        request.session['basket'] = basket
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Curry")
     return render (request, 'menu/curry.html', {'items': items, 'chosen_item': chosen_item} )
 
 def vegan (request):
+    basket = request.session.get('basket', [])
     chosen_item = None
     if request.method == "POST":
         item_id = request.POST.get ("item_id")
+        basket.append(item_id)
+        request.session['basket'] = basket
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Vegan")
     return render (request, 'menu/vegan.html', {'items': items, 'chosen_item': chosen_item} )
 
 def gluten_free (request):
+    basket = request.session.get('basket', [])
     chosen_item = None
     if request.method == "POST":
         item_id = request.POST.get ("item_id")
+        basket.append(item_id)
+        request.session['basket'] = basket
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Gluten Free")
     return render (request, 'menu/gluten_free.html', {'items': items, 'chosen_item': chosen_item} )
 
 def saver_menu (request):
+    basket = request.session.get('basket', [])
     chosen_item = None
     if request.method == "POST":
         item_id = request.POST.get ("item_id")
+        basket.append(item_id)
+        request.session['basket'] = basket
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Saver Menu")
-    return render (request, 'menu/saver_menu.html', {'items': items, 'chosen_item': chosen_item} )   
+    return render (request, 'menu/saver_menu.html', {'items': items, 'chosen_item': chosen_item} ) 
 
 def desserts (request):
+    basket = request.session.get('basket', [])
     chosen_item = None
     if request.method == "POST":
         item_id = request.POST.get ("item_id")
+        basket.append(item_id)
+        request.session['basket'] = basket
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Desserts")
     return render (request, 'menu/desserts.html', {'items': items, 'chosen_item': chosen_item} )
 
 def kids_menu (request):
+    basket = request.session.get('basket', [])
     chosen_item = None
     if request.method == "POST":
         item_id = request.POST.get ("item_id")
+        basket.append(item_id)
+        request.session['basket'] = basket
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Kids Menu")
-    return render (request, 'menu/kids_menu.html',{'items': items, 'chosen_item': chosen_item} )
+    return render (request, 'menu/kids_menu.html', {'items': items, 'chosen_item': chosen_item} )
+
+
+def basket (request):
+     basket = request.session.get('basket', [])
+     return render (request, 'menu/basket.html', {
+         'basket': basket
+     })
