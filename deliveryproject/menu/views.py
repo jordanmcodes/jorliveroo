5fa24840b18e7adc745651a65bba1a6b2dabfe97
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-05-22 09:22:03 +0100

    Fixed code in views.py, because when I clicked add on any page that wasn't burgers.html, nothing happened as I didn't add chosen_item to the end of the items code.

--- deliveryproject/menu/views.py
+++ deliveryproject/menu/views.py
@@ -19,7 +19,7 @@ def pasta (request):
         item_id = request.POST.get ("item_id")
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Pasta")
-    return render (request, 'menu/pasta.html', {'items': items})
+    return render (request, 'menu/pasta.html', {'items': items, 'chosen_item': chosen_item} )
 
 def pizza (request):
     chosen_item = None
@@ -27,7 +27,7 @@ def pizza (request):
         item_id = request.POST.get ("item_id")
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Pizza")
-    return render (request, 'menu/pizza.html', {'items': items})
+    return render (request, 'menu/pizza.html', {'items': items, 'chosen_item': chosen_item} )
 
 def fish (request):
     chosen_item = None
@@ -35,7 +35,7 @@ def fish (request):
         item_id = request.POST.get ("item_id")
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Fish")
-    return render (request, 'menu/fish.html', {'items': items})
+    return render (request, 'menu/fish.html', {'items': items, 'chosen_item': chosen_item} )
 
 def soup (request):
     chosen_item = None
@@ -43,7 +43,7 @@ def soup (request):
         item_id = request.POST.get ("item_id")
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Soup")
-    return render(request, 'menu/soup.html',{'items': items})
+    return render(request, 'menu/soup.html',{'items': items, 'chosen_item': chosen_item} )
 
 def sides (request):
     chosen_item = None
@@ -51,7 +51,7 @@ def sides (request):
         item_id = request.POST.get ("item_id")
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Sides")
-    return render (request, 'menu/sides.html', {'items': items})
+    return render (request, 'menu/sides.html', {'items': items, 'chosen_item': chosen_item} )
 
 def curry (request):
     chosen_item = None
@@ -59,7 +59,7 @@ def curry (request):
         item_id = request.POST.get ("item_id")
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Curry")
-    return render (request, 'menu/curry.html', {'items': items})
+    return render (request, 'menu/curry.html', {'items': items, 'chosen_item': chosen_item} )
 
 def vegan (request):
     chosen_item = None
@@ -67,7 +67,7 @@ def vegan (request):
         item_id = request.POST.get ("item_id")
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Vegan")
-    return render (request, 'menu/vegan.html', {'items': items})
+    return render (request, 'menu/vegan.html', {'items': items, 'chosen_item': chosen_item} )
 
 def gluten_free (request):
     chosen_item = None
@@ -75,7 +75,7 @@ def gluten_free (request):
         item_id = request.POST.get ("item_id")
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Gluten Free")
-    return render (request, 'menu/gluten_free.html', {'items': items} )
+    return render (request, 'menu/gluten_free.html', {'items': items, 'chosen_item': chosen_item} )
 
 def saver_menu (request):
     chosen_item = None
@@ -83,7 +83,7 @@ def saver_menu (request):
         item_id = request.POST.get ("item_id")
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Saver Menu")
-    return render (request, 'menu/saver_menu.html', {'items': items})   
+    return render (request, 'menu/saver_menu.html', {'items': items, 'chosen_item': chosen_item} )   
 
 def desserts (request):
     chosen_item = None
@@ -91,7 +91,7 @@ def desserts (request):
         item_id = request.POST.get ("item_id")
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Desserts")
-    return render (request, 'menu/desserts.html', {'items': items} )
+    return render (request, 'menu/desserts.html', {'items': items, 'chosen_item': chosen_item} )
 
 def kids_menu (request):
     chosen_item = None
@@ -99,4 +99,4 @@ def kids_menu (request):
         item_id = request.POST.get ("item_id")
         chosen_item = item_id
     items = FoodItem.objects.filter(category__name="Kids Menu")
-    return render (request, 'menu/kids_menu.html',{'items': items} )
+    return render (request, 'menu/kids_menu.html',{'items': items, 'chosen_item': chosen_item} )
