ae7df2f8561f7bc41614622043ee59f4f208b93e
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-05-22 20:27:03 +0100

    attempting to fix deployment issue.

--- deliveryproject/deliveryproject/wsgi.py
+++ deliveryproject/deliveryproject/wsgi.py
@@ -11,5 +11,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deliveryproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deliveryproject.deliveryproject.settings')
 application = get_wsgi_application()
