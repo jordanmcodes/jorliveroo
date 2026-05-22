8215961f685423d2af74b99b42da71e8dc5d1808
Marn39 <210750650+Marn39@users.noreply.github.com>
2026-05-22 19:54:56 +0100

    Changed settings file.

--- deliveryproject/deliveryproject/settings.py
+++ deliveryproject/deliveryproject/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "dummy key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
-
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
